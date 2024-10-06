258f3e60a8edef6b4b19abf327efc30f5c4be532
isthatXinyu <112805093+isthatXinyu@users.noreply.github.com>
2024-10-06 16:52:07 +1000

    ploading it to S3 using PutObjectCommand

--- s3.js
+++ s3.js
@@ -4,20 +4,25 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const bucketName = 'n10366687-test';
 const s3Client = new S3Client({ region: 'ap-southeast-2' });
 
-async function uploadToS3(objectKey, objectValue) {
+const fs = require('fs');
+const uploadToS3 = async (s3Key, localFilePath) => {
     try {
-        const response = await s3Client.send(new PutObjectCommand({
+        const fileStream = fs.createReadStream(localFilePath);
+        const uploadParams = {
             Bucket: bucketName,
-            Key: objectKey,
-            Body: objectValue
-        }));
-        console.log('Write Response:', response);
+            Key: s3Key,
+            Body: fileStream,
+            ContentType: 'video/mp4', // or other format
+        };
+        const response = await s3Client.send(new PutObjectCommand(uploadParams));
+        console.log(`Transcoded video uploaded to S3 with key: ${s3Key}`);
         return response;
     } catch (err) {
-        console.error('Error writing to S3:', err);
+        console.error('Error uploading transcoded video to S3:', err);
         throw err;
     }
-}
+};
+
 
 async function generatePresignedUrl(objectKey) {
     try {
